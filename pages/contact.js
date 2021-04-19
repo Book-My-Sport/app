a0d27a23eb31be71bd0a68054bb84d3165eeb28c
bookmysport <80315862+bookmysport@users.noreply.github.com>
2021-04-19 10:17:22 +0200

    Update for app launching

---
+++ pages/contact.js
@@ -0,0 +1,17 @@
+export default function contactPage() {
+    return (
+        <h1>Page contact</h1>
+
+        // <select name="place" id="place">
+        // <option value="">Choisir la salle de sport</option>
+        // </select>
+
+        <select
+        
+        />
+
+        )
+
+
+}
+
---
+++ pages/test.js
@@ -0,0 +1,15 @@
+export default function contactPage() {
+    return (
+
+        <div>
+            <h1>Page contact</h1>
+
+            <select name="place" id="place">
+                <option value="">Choisir la salle de sport</option>
+            </select>
+        </div>
+
+        )
+
+
+}
