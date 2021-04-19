a60b0c08658bada7270507c17949cb96b2969ed0
bookmysport <80315862+bookmysport@users.noreply.github.com>
2021-04-19 10:32:07 +0200

    Update test.js

--- pages/test.js
+++ pages/test.js
@@ -2,7 +2,7 @@ export default function contactPage() {
     return (
 
         <div>
-            <h1>Page contact</h1>
+            <h1>Page test</h1>
 
             <select name="place" id="place">
                 <option value="">Choisir la salle de sport</option>
