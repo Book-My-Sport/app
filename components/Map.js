7148150c6662f2279b231ee9be124d6f51a42fa9
bookmysport <80315862+bookmysport@users.noreply.github.com>
2021-04-19 11:26:47 +0200

    Update Map

---
+++ components/Map.js
@@ -0,0 +1,83 @@
+// import { Component } from 'react';
+import * as React from 'react';
+import {Component} from 'react';
+import {render} from 'react-dom';
+import MapGL, {Source, Layer} from 'react-map-gl';
+
+import ControlPanel from './control-panel';
+import {clusterLayer, clusterCountLayer, unclusteredPointLayer} from './layers';
+
+const MAPBOX_TOKEN = 'pk.eyJ1IjoiYm9vay1teS1zcG9ydCIsImEiOiJja25vOWxpcDExOXk1MnVwZXl3c24zNHZzIn0.Z0l7ZiXiaxJGYpNcwGF-Vw'; // Set your mapbox token here
+
+class Map extends Component {
+  state = {
+    viewport: {
+      width: '100vw',
+      height: '100vh',
+      latitude: 50.6965038397,
+      longitude: 3.15838315346,
+      zoom: 10
+    }
+  };
+
+  _sourceRef = React.createRef();
+
+  _onViewportChange = viewport => this.setState({viewport});
+
+//   _onClick = event => {
+//     const feature = event.features[0];
+//     const clusterId = feature.properties.cluster_id;
+
+//     const mapboxSource = this._sourceRef.current.getSource();
+
+//     mapboxSource.getClusterExpansionZoom(clusterId, (err, zoom) => {
+//       if (err) {
+//         return;
+//       }
+
+//       this._onViewportChange({
+//         ...this.state.viewport,
+//         longitude: feature.geometry.coordinates[0],
+//         latitude: feature.geometry.coordinates[1],
+//         zoom,
+//         transitionDuration: 500
+//       });
+//     });
+//   };
+
+  render() {
+    return (
+    //   <ReactMapGL
+    //     mapStyle="mapbox://styles/mapbox/streets-v9"
+    //     mapboxApiAccessToken="pk.eyJ1IjoiYm9vay1teS1zcG9ydCIsImEiOiJja25vOWxpcDExOXk1MnVwZXl3c24zNHZzIn0.Z0l7ZiXiaxJGYpNcwGF-Vw"
+    //     onViewportChange={(viewport) => this.setState({ viewport })}
+    //     {...this.state.viewport}
+    //   />
+
+    <MapGL
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+        mapboxApiAccessToken="pk.eyJ1IjoiYm9vay1teS1zcG9ydCIsImEiOiJja25vOWxpcDExOXk1MnVwZXl3c24zNHZzIn0.Z0l7ZiXiaxJGYpNcwGF-Vw"
+        onViewportChange={(viewport) => this.setState({ viewport })}
+        {...this.state.viewport}
+        interactiveLayerIds={[clusterLayer.id]}
+        // onClick={this._onClick}
+      >
+        <Source
+          type="geojson"
+          data="https://docs.mapbox.com/mapbox-gl-js/assets/earthquakes.geojson"
+          cluster={true}
+          clusterMaxZoom={14}
+          clusterRadius={50}
+          ref={this._sourceRef}
+        >
+          <Layer {...clusterLayer} />
+          <Layer {...clusterCountLayer} />
+          <Layer {...unclusteredPointLayer} />
+        </Source>
+        {/* <ControlPanel containerComponent={this.props.containerComponent} /> */}
+      </MapGL>
+    );
+  }
+}
+
+export default Map;
--- pages/head.js
+++ pages/head.js
@@ -1,9 +1,9 @@
 import Head from 'next/head'
 
-export default function contactPage() {
-    return (
-        <>
-        <Head>
+export default function Home() {
+  return (
+    <div>
+      <Head>
         <title>This page has a title 🤔</title>
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
@@ -11,20 +11,7 @@ export default function contactPage() {
         <link href='https://api.mapbox.com/mapbox-gl-js/v2.1.1/mapbox-gl.css' rel='stylesheet' />
       </Head>
 
-
-        <div>
-            <h1>Page test</h1>
-
-            <select name="place" id="place">
-                <option value="">Choisir la salle de sport</option>
-            </select>
-        </div>
-
-        <div id='map' ></div>
-        </>
-
-)
-
-
-
+      <h1>This page has a title 🤔</h1>
+    </div>
+  )
 }
