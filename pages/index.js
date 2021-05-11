23904c8e0a7347382b6abee5b8092ce0f97090b2
LuckyMarty <62702759+LuckyMarty@users.noreply.github.com>
2021-05-11 09:08:45 +0200

    Header Added

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,15 @@
-import dynamic from 'next/dynamic';
+// import dynamic from 'next/dynamic';
 
-const DynamicComponentWithNoSSR = dynamic(() => import('../components/Map'), {
-  ssr: false
-});
+// const DynamicComponentWithNoSSR = dynamic(() => import('../components/Map'), {
+//   ssr: false
+// });
 
-export default () => <DynamicComponentWithNoSSR />;
+// export default () => <DynamicComponentWithNoSSR />;
+
+import Header from '../components/Header'
+
+export default function Home() {
+  return (
+    <Header />
+  )
+}
