5f92bbf2d8a5d481c249e352950792e90508ee4d
bookmysport <80315862+bookmysport@users.noreply.github.com>
2021-05-25 17:16:06 +0200

    Error Confused Update

--- components/Steps.js
+++ components/Steps.js
@@ -10,28 +10,19 @@ const Steps = () => {
 
             <div>
                 <div className="onestep">2</div>
-<<<<<<< Updated upstream
                 <div>Choix de la salle</div>
-=======
-                <div>Choisir sa salle de sport</div>
->>>>>>> Stashed changes
             </div>
 
             <div>
                 <div className="onestep">3</div>
-<<<<<<< Updated upstream
                 <div>Réservation</div>
-            </div>
-
-=======
-                <div>Récapitulatif</div>
             </div>
 
             <div>
                 <div className="onestep">4</div>
                 <div>Réserver</div>
             </div>
->>>>>>> Stashed changes
+
         </div>
     )
 }
--- pages/step-2.js
+++ pages/step-2.js
@@ -6,7 +6,6 @@ const DynamicComponentWithNoSSR = dynamic(() => import('../components/Map'), {
 
 import Header from '../components/Header'
 import Steps from '../components/Steps'
-import Testmap from '../components/Testmap'
 
 export default function Home() {
   return (
@@ -31,14 +30,9 @@ export default function Home() {
         </div>
 
         <DynamicComponentWithNoSSR />
-        <Testmap />
 
         <button className="retour"><a href="/">Retour</a></button>
-        <button className="continuer" {
-          constructor() {
-
-          }
-        }><a href="/step-3.js">Continuer</a></button>
+        <button className="continuer"><a href="/step-3.js">Continuer</a></button>
       </form>
     </main>
 
