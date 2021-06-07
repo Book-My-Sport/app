eb3c2019116adf23b7e8ad90dd92718f39ad2da3
Benoit Maigret <dev@benoitmaigret.com>
2021-06-07 09:59:44 +0200

    Create a popup to Mapbox

--- components/Map.js
+++ components/Map.js
@@ -2,7 +2,7 @@
 import * as React from 'react';
 import {Component} from 'react';
 import {render} from 'react-dom';
-import MapGL, {Source, Layer} from 'react-map-gl';
+import MapGL, {Popup, Source, Layer} from 'react-map-gl';
 
 import ControlPanel from './control-panel';
 import {clusterLayer, clusterCountLayer, unclusteredPointLayer} from './layers';
@@ -12,20 +12,30 @@ import {clusterLayer, clusterCountLayer, unclusteredPointLayer} from './layers';
 const MAPBOX_TOKEN = 'pk.eyJ1IjoiYm9vay1teS1zcG9ydCIsImEiOiJja25vOWxpcDExOXk1MnVwZXl3c24zNHZzIn0.Z0l7ZiXiaxJGYpNcwGF-Vw'; // Set your mapbox token here
 
 class Map extends Component {
-  state = {
-    viewport: {
-      width: '100%',
-      height: '500px',
-      latitude: 50.6965038397,
-      longitude: 3.15838315346,
-      zoom: 10
-    }
-  };
+  constructor(props) {
+    super(props)
+    this.state = {
+      viewport: {
+        width: '100%',
+        height: '500px',
+        latitude: 50.6965038397,
+        longitude: 3.15838315346,
+        zoom: 10
+      },
+      popupInfo: null
+    };
+  }
 
   _sourceRef = React.createRef();
 
+  _onViewportChange = viewport => this.setState({viewport});
+
   _onClick = event => {
     const feature = event.features[0];
+    if (feature.layer.id === 'unclustered-point') {
+      this.setState({ popupInfo: feature.properties }) 
+      return
+    }
     const clusterId = feature.properties.cluster_id;
     const mapboxSource = this._sourceRef.current.getMap().getSource('sport');
 
@@ -46,6 +56,13 @@ class Map extends Component {
 
   render() {
     return (
+    //   <ReactMapGL
+    //     mapStyle="mapbox://styles/mapbox/streets-v9"
+    //     mapboxApiAccessToken="pk.eyJ1IjoiYm9vay1teS1zcG9ydCIsImEiOiJja25vOWxpcDExOXk1MnVwZXl3c24zNHZzIn0.Z0l7ZiXiaxJGYpNcwGF-Vw"
+    //     onViewportChange={(viewport) => this.setState({ viewport })}
+    //     {...this.state.viewport}
+    //   />
+
     <MapGL
         mapStyle="mapbox://styles/mapbox/streets-v9"
         mapboxApiAccessToken="pk.eyJ1IjoiYm9vay1teS1zcG9ydCIsImEiOiJja25vOWxpcDExOXk1MnVwZXl3c24zNHZzIn0.Z0l7ZiXiaxJGYpNcwGF-Vw"
@@ -69,6 +86,19 @@ class Map extends Component {
           <Layer {...clusterLayer} />
           <Layer {...clusterCountLayer} />
           <Layer {...unclusteredPointLayer} />
+          {this.state.popupInfo && (
+            <Popup
+              tipSize={5}
+              anchor="top"
+              longitude={Number(this.state.popupInfo.long)}
+              latitude={Number(this.state.popupInfo.lat)}
+              closeOnClick={true}
+              onClose={()=>this.setState({popupInfo: null})}
+            >
+              <h2>{this.state.popupInfo.nom}</h2>
+              <p>{this.state.popupInfo.adresse}</p>
+            </Popup>
+          )}
         </Source>
         {/* <ControlPanel containerComponent={this.props.containerComponent} /> */}
       </MapGL>
