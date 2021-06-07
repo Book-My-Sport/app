4fda8e628ced1c52cd23689daf0bdc319ab3e3ce
Cassandre Ldl <CassandreLdl@github.com>
2021-06-07 08:37:32 +0200

    Create Step3

--- pages/step-3.js
+++ pages/step-3.js
@@ -17,7 +17,18 @@ export default function Home() {
         <Steps />
 
         <div className="deux trois">
-          <p></p>
+          <h2>Bonjour<?php $_POST["nom"] $_POST["prenom"]?>, </h2>
+          <h3>Vos informations personnelles :</h3>
+          <p>E-mail : <?php $_POST["mail"]?><br>
+          Tel : <?php $_POST["tel"]?><br>
+          Adresse : <?php $_POST["adresse"]?><br>
+          Complément d'adresse : <?php $_POST["adressecomplement"]?><br>
+          Code postal : <?php $_POST["codepostal"]?><br>
+          Ville : <?php $_POST["ville"]?><br></p>
+          <h3>Votre réservation :</h3>
+          <p>Vous avez réservé la salle ... à ...<br>
+          Pour le <?php $_POST["date"]?>
+          à <?php $_POST["heure"]?>
         </div>
     </main>
 
